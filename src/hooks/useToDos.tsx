85cff6f0c962612cca4246a51dd115c61fb825b8
Steven Isbell <steveni_92@msn.com>
2018-11-08 11:59:04 -0600

    hook

---
+++ src/hooks/useToDos.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+type ToDo = {
+  id: number;
+  text: string;
+  checked: boolean;
+};
+
+export const useToDos = (initialValue = []) => {
+  const [todos, setTodos] = useState(initialValue);
+  const [id, setId] = useState(0);
+
+  return {
+    todos,
+    addTodo(text: string) {
+      if (text) {
+        setTodos([...todos, { id, text, checked: false }]);
+        setId(id + 1);
+      }
+    },
+    checkTodo(id: number) {
+      setTodos(
+        todos.map((todo: ToDo) => {
+          if (todo.id === id) {
+            todo.checked = !todo.checked;
+          }
+          return todo;
+        })
+      );
+    },
+    removeTodo(id: number) {
+      setTodos(todos.filter((todo: ToDo) => todo.id !== id));
+    }
+  };
+};
