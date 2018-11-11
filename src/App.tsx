2cc0bf54b730a1b6d1d3fe53ed687fe95249fd78
Steven Isbell <steveni_92@msn.com>
2018-11-11 09:40:54 -0700

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import Header from './components/Header';
 
+import Header from './components/Header';
 import AddToDo from './components/AddTodo';
-import ToDoList from './components/ToDoList';
+import Loader from './components/Loader';
 
 import useInputValue from './hooks/useInputValue';
 import useToDos from './hooks/useToDos';
+
+import './App.css';
+// import ToDoList from './components/ToDoList';
+
+const ToDoList = React.lazy(() => import('./components/ToDoList'));
 
 const App = () => {
   const { inputValue, changeInput, clearInput, handleEnter } = useInputValue();
   const { todos, addTodo, removeTodo, checkTodo } = useToDos();
 
   return (
-    <Paper
-      elevation={0}
-      style={{ padding: 0, margin: 0, backgroundColor: '#fafafa' }}
-    >
-      <Header />
-      <AddToDo
-        value={inputValue}
-        handleInputChange={changeInput}
-        handleEnter={handleEnter}
-        handleAddTodo={addTodo}
-        clearInput={clearInput}
-      />
-      <ToDoList
-        items={todos}
-        handleItemCheck={checkTodo}
-        handleItemDelete={removeTodo}
-      />
-    </Paper>
+    <React.Suspense fallback={<h1>Loading...</h1>}>
+      <Paper
+        elevation={0}
+        style={{ padding: 0, margin: 0, backgroundColor: 'inherit' }}
+      >
+        <Header />
+        <div style={{ maxWidth: 1200, margin: '0 auto' }}>
+          <AddToDo
+            value={inputValue}
+            handleInputChange={changeInput}
+            handleEnter={handleEnter}
+            handleAddTodo={addTodo}
+            clearInput={clearInput}
+          />
+          <Loader />
+          <ToDoList
+            items={todos}
+            handleItemCheck={checkTodo}
+            handleItemDelete={removeTodo}
+          />
+        </div>
+      </Paper>
+    </React.Suspense>
   );
 };
 
---
+++ src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="progress container">
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default Loader;
