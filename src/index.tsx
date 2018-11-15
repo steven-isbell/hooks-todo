53ab55467dec113248c559f4c355f8a0cc324430
Steven Isbell <steveni_92@msn.com>
2018-11-14 21:52:45 -0700

    remove files

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,3 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 // No TS Support -> Can't use concurrent mode in current types version -> no maxDuration with suspense
 // ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
