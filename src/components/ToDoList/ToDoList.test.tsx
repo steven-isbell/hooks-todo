9c23111e7c62092838d658e30256c6cfebfa5ab9
Steven Isbell <steveni_92@msn.com>
2018-11-19 23:48:13 -0700

    tests

--- src/components/ToDoList/ToDoList.test.tsx
+++ src/components/ToDoList/ToDoList.test.tsx
@@ -16,3 +16,20 @@ test('ToDoList should render with props', () => {
 
   render(<ToDoList {...props} />);
 });
+
+test('ToDoList renders the right text for each item', () => {
+  const items = [{ id: 1, text: 'Hello World', completed: false }];
+  const handleItemDelete = jest.fn();
+  const handleItemCheck = jest.fn();
+
+  const props = {
+    items,
+    handleItemDelete,
+    handleItemCheck
+  };
+
+  const { getByText } = render(<ToDoList {...props} />);
+
+  const todoText = getByText(props.items[0].text);
+  expect(todoText).toBeDefined();
+});
